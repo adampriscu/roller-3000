22d1e023f3a618f5b34cde3504c29e24d048503c
Adam Priscu <adam.priscu@gmail.com>
2026-07-09 00:24:11 +0300

    Update paint recipes

--- src/palette.js
+++ src/palette.js
@@ -5,48 +5,46 @@
 
 // Base paints, keyed by the dot colour used in the guide's recipes.
 export const BASE_PAINTS = {
-  white:   "#FFFFFF",
-  red:     "#FF0000",
-  blue:    "#1B4DA8",
-  black:   "#111111",
-  yellow:  "#FFE100",
-  magenta: "#EC1E9C",
-  green:   "#22B14C",
+  white:  "#FFFFFF",
+  red:    "#FF0000",
+  blue:   "#1B4DA8",
+  black:  "#111111",
+  yellow: "#FFE100",
 };
 
 // Which dots need a light number for contrast.
-export const DARK_PAINTS = new Set(["blue", "black", "magenta"]);
+export const DARK_PAINTS = new Set(["blue", "black"]);
 
 export const PALETTE = [
   {
     id: 1, hex: "040B1A", rgb: [4, 11, 26],
     hsb: [221, 85, 10], cmyk: [84, 57, 0, 89],
-    recipe: [{ paint: "blue", parts: 1 }, { paint: "black", parts: 4 }],
+    recipe: [{ paint: "blue", parts: 1 }, { paint: "black", parts: 6 }],
   },
   {
     id: 2, hex: "BD2940", rgb: [189, 41, 64],
     hsb: [351, 78, 74], cmyk: [0, 78, 66, 25],
-    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 5 }, { paint: "blue", parts: 1 }],
+    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 5 }],
   },
   {
     id: 3, hex: "3A1727", rgb: [58, 23, 39],
     hsb: [333, 60, 23], cmyk: [0, 60, 32, 77],
-    recipe: [{ paint: "yellow", parts: 1 }, { paint: "red", parts: 5 }, { paint: "blue", parts: 3 }, { paint: "black", parts: 4 }],
+    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 7 }, { paint: "black", parts: 1 }, { paint: "blue", parts: 1 }],
   },
   {
     id: 4, hex: "0F3963", rgb: [15, 57, 99],
     hsb: [210, 85, 39], cmyk: [84, 42, 0, 61],
-    recipe: [{ paint: "yellow", parts: 2 }, { paint: "red", parts: 1 }, { paint: "blue", parts: 6 }, { paint: "black", parts: 2 }],
+    recipe: [{ paint: "yellow", parts: 2 }, { paint: "red", parts: 1 }, { paint: "blue", parts: 10 }],
   },
   {
     id: 5, hex: "121B31", rgb: [18, 27, 49],
     hsb: [223, 63, 19], cmyk: [63, 44, 0, 80],
-    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 1 }, { paint: "black", parts: 9 }, { paint: "blue", parts: 4 }],
+    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 1 }, { paint: "blue", parts: 9 }, { paint: "black", parts: 4 }],
   },
   {
     id: 7, hex: "661124", rgb: [102, 17, 36],
     hsb: [347, 83, 40], cmyk: [0, 83, 64, 60],
-    recipe: [{ paint: "yellow", parts: 2 }, { paint: "red", parts: 1 }, { paint: "magenta", parts: 2 }, { paint: "black", parts: 3 }],
+    recipe: [{ paint: "white", parts: 2 }, { paint: "red", parts: 5 }, { paint: "black", parts: 1 }],
   },
   {
     id: 8, hex: "030206", rgb: [3, 2, 6],
@@ -56,7 +54,7 @@ export const PALETTE = [
   {
     id: 9, hex: "465C7E", rgb: [70, 92, 126],
     hsb: [216, 44, 49], cmyk: [44, 26, 0, 50],
-    recipe: [{ paint: "white", parts: 1 }, { paint: "green", parts: 3 }, { paint: "magenta", parts: 2 }, { paint: "blue", parts: 3 }],
+    recipe: [{ paint: "white", parts: 4 }, { paint: "black", parts: 6 }, { paint: "blue", parts: 5 }],
   },
 ];
 
